5e27eb5ed1f5f3d9f4ab4905067af53f124a887c
ata kaleli <atakaleli123@gmail.com>
2025-03-26 17:24:53 +0300

    Animation Controller script is created
    Idle adn Run animations are created and added into HandleMovement

--- Assets/_Scripts/Agent.cs
+++ Assets/_Scripts/Agent.cs
@@ -1,35 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Agent : MonoBehaviour
 {
     private PlayerInput playerInput;
     private Rigidbody2D rb;
+    private AnimationController anim;
 
-
+    private bool isStartRunning = false;
 
     private void Awake()
     {
         playerInput = GetComponentInParent<PlayerInput>();
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponentInChildren<AnimationController>();
     }
 
     private void OnEnable()
     {
         playerInput.OnMovement += HandleMovement;
+        
     }
 
 
     private void HandleMovement(Vector2 input)
     {
+       
         if(Mathf.Abs(input.x) > 0)
         {
+            if(Mathf.Abs(rb.velocity.x) < 0.01f)
+            {
+                anim.PlayAnimation(AnimationType.Run);
+            }
+
             rb.velocity = new Vector2(input.x * 5f, rb.velocity.y);
         }
         else
         {
+            if (Mathf.Abs(rb.velocity.x) > 0.01f)
+            {
+                anim.PlayAnimation(AnimationType.Idle);
+            }
+
             rb.velocity = new Vector2(0, rb.velocity.y);
         }
+
+        
+        
     }
 }
--- Assets/_Scripts/AnimationController.cs
+++ Assets/_Scripts/AnimationController.cs
@@ -1,18 +1,42 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+
+
+public enum AnimationType
+{
+    Idle,
+    Run
+}
 
 public class AnimationController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Animator anim;
+
+
+
+    private void Awake()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void PlayAnimation(AnimationType animationType)
     {
-        
+        switch (animationType)
+        {
+            case AnimationType.Idle:
+                Play("Player_Idle");
+                break;
+            case AnimationType.Run:
+                Play("Player_Run");
+                break;
+            default:
+                print("No Animation found");
+                break;
+        }
+    }
+
+    private void Play(string name)
+    {
+        anim.Play(name, -1, 0f);
     }
 }
+
